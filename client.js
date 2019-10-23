978027640d908da27461f0d86b0b10df397ba94c
Jeff Gebert <jeffgebert@hotmail.com>
2019-10-23 17:35:52 +0000

    added initials from client side

--- client.js
+++ client.js
@@ -7,14 +7,30 @@ const connect = function() {
   const conn = net.createConnection({ 
     host: 'localhost',
     port: 50541
+    
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+  conn.on('connect', (conn) => {
+    console.log("Successfully connected to game server" );
+  });
+
+  conn.on('connect', () => {
+    conn.write("Name: JSG");
+    conn.write('Hello from client!');
+  });
+
+
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
+
+
 
   return conn;
 }
 
 module.exports = {connect}
+
+
